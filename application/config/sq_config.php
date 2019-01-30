41fede8a569776b6f3495de299379daa18b23567
amardeep18 <amardeep.cotocus@gmail.com>
2019-01-30 17:59:55 +0530

    adding updated project

--- application/config/sq_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-                $sq_base_url='http://localhost/quiz/';
-                $sq_hostname='localhost';
-                $sq_dbname='unlimite_quiz1';
-                $sq_dbusername='root';
-                $sq_dbpassword='';
-?>
-
